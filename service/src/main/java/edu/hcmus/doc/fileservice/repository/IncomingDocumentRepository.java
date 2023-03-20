8694c7b93cf52552251242d7d6e08b2c54391bc5
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2023-03-20 15:55:55 +0700

    [DOC-145] search criteria done

--- service/src/main/java/edu/hcmus/doc/fileservice/repository/IncomingDocumentRepository.java
+++ service/src/main/java/edu/hcmus/doc/fileservice/repository/IncomingDocumentRepository.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IncomingDocumentRepository extends ElasticsearchRepository<IncomingDocument, String> {
-  List<IncomingDocument> findByIncomingNumberOrOriginalSymbolNumberOrSummary(String incomingNumber, String originalSymbolNumber, String summary);
+  List<IncomingDocument> findByIncomingNumber(String incomingNumber);
 
+  List<IncomingDocument> findByOriginalSymbolNumber(String originalSymbolNumber);
+
+  List<IncomingDocument> findBySummary(String summary);
+
+  List<IncomingDocument> findByIncomingNumberAndOriginalSymbolNumber(String incomingNumber, String originalSymbolNumber);
+
+  List<IncomingDocument> findByIncomingNumberAndSummary(String incomingNumber, String summary);
+
+  List<IncomingDocument> findByOriginalSymbolNumberAndSummary(String originalSymbolNumber, String summary);
+
+  List<IncomingDocument> findByIncomingNumberAndOriginalSymbolNumberAndSummary(String incomingNumber, String originalSymbolNumber, String summary);
 }
--- service/src/main/java/edu/hcmus/doc/fileservice/service/impl/IncomingDocumentServiceImpl.java
+++ service/src/main/java/edu/hcmus/doc/fileservice/service/impl/IncomingDocumentServiceImpl.java
@@ -1,7 +1,5 @@
 package edu.hcmus.doc.fileservice.service.impl;
 
-import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
-
 import edu.hcmus.doc.fileservice.model.dto.IncomingDocumentCriteriaDto;
 import edu.hcmus.doc.fileservice.model.entity.IncomingDocument;
 import edu.hcmus.doc.fileservice.repository.IncomingDocumentRepository;
@@ -9,10 +7,7 @@ import edu.hcmus.doc.fileservice.service.IncomingDocumentService;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,24 +15,37 @@ import org.springframework.stereotype.Service;
 public class IncomingDocumentServiceImpl implements IncomingDocumentService {
   private final IncomingDocumentRepository incomingDocumentRepository;
 
-  private final ElasticsearchOperations operations;
-
-
   @Override
   public List<IncomingDocument> getIncomingDocuments(IncomingDocumentCriteriaDto incomingDocumentCriteriaDto) {
-//    var query = new NativeSearchQueryBuilder()
-//        .withQuery(queryStringQuery("incoming_number:" + incomingDocumentCriteriaDto.getIncomingNumber()))
-//        .build();
-//
-//
-//    SearchHits<IncomingDocument> incomingDocumentHits = operations.search(query, IncomingDocument.class, IndexCoordinates.of("logstash-incoming-document-index"));
-//
-//    List<IncomingDocument> incomingDocumentMatches = new ArrayList<>();
-//    incomingDocumentHits.forEach(searchHit->{
-//      incomingDocumentMatches.add(searchHit.getContent());
-//    });
-//    return incomingDocumentMatches;
-    return incomingDocumentRepository.findByIncomingNumberOrOriginalSymbolNumberOrSummary(incomingDocumentCriteriaDto.getIncomingNumber(), incomingDocumentCriteriaDto.getOriginalSymbolNumber(), incomingDocumentCriteriaDto.getSummary());
+    if(incomingDocumentCriteriaDto != null) {
+      if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getIncomingNumber()) && StringUtils.isNotBlank(incomingDocumentCriteriaDto.getOriginalSymbolNumber()) && StringUtils.isNotBlank(incomingDocumentCriteriaDto.getSummary())) {
+        return incomingDocumentRepository.findByIncomingNumberAndOriginalSymbolNumberAndSummary(incomingDocumentCriteriaDto.getIncomingNumber(), incomingDocumentCriteriaDto.getOriginalSymbolNumber(), incomingDocumentCriteriaDto.getSummary());
+      } else {
+        if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getIncomingNumber()) && StringUtils.isNotBlank(incomingDocumentCriteriaDto.getOriginalSymbolNumber())) {
+          return incomingDocumentRepository.findByIncomingNumberAndOriginalSymbolNumber(incomingDocumentCriteriaDto.getIncomingNumber(), incomingDocumentCriteriaDto.getOriginalSymbolNumber());
+        } else {
+          if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getIncomingNumber()) && StringUtils.isNotBlank(incomingDocumentCriteriaDto.getSummary())) {
+            return incomingDocumentRepository.findByIncomingNumberAndSummary(incomingDocumentCriteriaDto.getIncomingNumber(), incomingDocumentCriteriaDto.getSummary());
+          } else {
+            if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getOriginalSymbolNumber()) && StringUtils.isNotBlank(incomingDocumentCriteriaDto.getSummary())) {
+              return incomingDocumentRepository.findByOriginalSymbolNumberAndSummary(incomingDocumentCriteriaDto.getOriginalSymbolNumber(), incomingDocumentCriteriaDto.getSummary());
+            } else {
+              if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getIncomingNumber())) {
+                return incomingDocumentRepository.findByIncomingNumber(incomingDocumentCriteriaDto.getIncomingNumber());
+              } else {
+                if(StringUtils.isNotBlank(incomingDocumentCriteriaDto.getOriginalSymbolNumber())) {
+                  return incomingDocumentRepository.findByOriginalSymbolNumber(incomingDocumentCriteriaDto.getOriginalSymbolNumber());
+                } else {
+                  return incomingDocumentRepository.findBySummary(incomingDocumentCriteriaDto.getSummary());
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return new ArrayList<>();
   }
 
 
