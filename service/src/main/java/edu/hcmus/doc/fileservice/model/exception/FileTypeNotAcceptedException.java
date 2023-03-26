a67bf5c116c3cd9afb3e4fc54dbf73e9d2a7801a
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-03-26 11:48:00 +0700

    [DOC-112] Update error messages

--- service/src/main/java/edu/hcmus/doc/fileservice/model/exception/FileTypeNotAcceptedException.java
+++ service/src/main/java/edu/hcmus/doc/fileservice/model/exception/FileTypeNotAcceptedException.java
@@ -2,7 +2,7 @@ package edu.hcmus.doc.fileservice.model.exception;
 
 public class FileTypeNotAcceptedException extends RuntimeException {
 
-  public static final String FILE_TYPE_NOT_ACCEPTED = "file_type_not_accepted";
+  public static final String FILE_TYPE_NOT_ACCEPTED = "file.file_type_not_accepted";
 
   public FileTypeNotAcceptedException(String message) {
     super(message);
